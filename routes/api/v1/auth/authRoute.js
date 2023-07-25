21f5b4a557b1d6c2d5941987d5f1960e3132d436
IlyaLevchenko04 <nneo2086@gmail.com>
2023-07-25 16:49:02 +0300

    swagger fixed

--- routes/api/v1/auth/authRoute.js
+++ routes/api/v1/auth/authRoute.js
@@ -16,15 +16,24 @@ const { auth } = require('../../../../middlewares/authMiddlware');
  *       - Auth
  *     summary: create user.
  *     description: Method for creating user. Required fields name, email, password.
-
+ *     parameters:
+ *       - in: body
+ *         name: email
+ *         required: true
+ *       - in: body
+ *         name: password
+ *         required: true
+ *       - in: body
+ *         name: name
+ *         required: true
  *
  *     responses:
  *       201:
  *         description: Returns created user.
- * 
+ *
  *       409:
  *         description: email in use.
- * 
+ *
  *       400:
  *         description: missing fields.
  *
@@ -40,15 +49,21 @@ router.post('/register', registerNewUser);
  *       - Auth
  *     summary: login in account of user.
  *     description: Method for login in account of user. Required fields email, password.
-
+ *     parameters:
+ *       - in: body
+ *         name: email
+ *         required: true
+ *       - in: body
+ *         name: password
+ *         required: true
  *
  *     responses:
  *       201:
  *         description: Returns logged user.
- * 
+ *
  *       400:
  *         description: missing fields.
- * 
+ *
  */
 
 router.post('/login', loginUser);
@@ -78,10 +93,14 @@ router.post('/logout', auth, logoutUser);
  *       - Auth
  *     summary: refresh tokens .
  *     description: Method for refreshing tokens.
+ *     parameters:
+ *       - in: body
+ *         name: refreshToken
+ *         required: true
  *
  *     responses:
  *       201:
- *         description: Returns new pair of tokens % acsessToken, refreshToken%.
+ *         description: Returns new pair of tokens acsessToken, refreshToken.
  *
  */
 
